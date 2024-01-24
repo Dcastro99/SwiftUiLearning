225e1e47790ca176a416b213e7e4b821ec80eb60
Danny <93397389+Dcastro99@users.noreply.github.com>
2024-01-24 13:35:56 -0800

    Initial Commit

---
+++ swiftui_test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftui_test
+//
+//  Created by Danny Castro on 1/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftui_test/swiftui_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_testApp.swift
+//  swiftui_test
+//
+//  Created by Danny Castro on 1/24/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
